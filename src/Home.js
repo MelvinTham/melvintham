6f1d26ac4067d9b1a7660e8cf619e972649367b5
MelvinTham <melvintham1997@gmail.com>
2018-07-29 11:17:28 -0400

    Resumed Website

--- src/Home.js
+++ src/Home.js
@@ -6,25 +6,16 @@ import CardContent from '@material-ui/core/CardContent';
 // import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import {
-  Button,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+import nyc from './images/nyc.jpg';
+import melvin from './images/Melvin.JPG';
+import {Image} from 'react-bootstrap';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     
     this.downButton = this.downButton.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
     
   }
   
@@ -37,32 +28,43 @@ class Home extends Component {
     r1.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     // document.getElementById('div').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
+
+handleScroll(){
+  var nav = document.getElementById('navButtons');
+  var sticky = nav.offsetTop;
+  if (window.pageYOffset >= sticky) {
+    console.log("it works!");
+    nav.classList.add("sticky");
+  } else {
+    console.log("it doesnt works!");
+    nav.classList.remove("sticky");
+  }
+}
   componentDidMount(){
     document.documentElement.addEventListener('scroll', this.handleScroll);
   }
 
   render() {
 
-    return (
-      
-      <div  data-aos ="fade-up">
-           <div id="nav" className ="navButtons">
-          <span class ="prev"></span>  
-          <span class ="next"></span>
-      </div> 
-       {/* <Navbar style={{height:'80px',backgroundColor:'black'}} expand="md">
-          <NavbarBrand style={{color: 'white',fontSize:'25px',fontFamily:'Roboto'}}  href="/">Melvin Tham</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-              <NavItem className="mr-5">
-                <NavLink style={{color: 'white',fontFamily:'Roboto'}} href="#">About</NavLink>
-              </NavItem>
-              <NavItem className="mr-5">
-                <NavLink style={{color: 'white',fontFamily:'Roboto'}}  href="#">Contact</NavLink>
-              </NavItem>
-            </Nav>
-        </Navbar>  */}
+    var textStyle = {
+      position: 'absolute', 
+      top: '25%', 
+      left: '35%',
+      color:'white',
+    };
 
-       <div id="div-01" className="background">
+
+    return (
+      <div  data-aos ="fade-down"  data-aos-easing="linear" data-aos-duration="750">
+          <div id="nav" className ="navButtons">
+          <span className ="prev"></span>  
+          <span className ="next"></span>
+          </div> 
+    
+
+       <div id="div-01">
+          <img src = {nyc} alt = "nyc" className ="nycHeight" data-aos="fade-down" />
+          <h1 style = {textStyle} data-aos="fade-down">hello My Name is Melvin</h1>
        {/* <button onClick= {this.downButton} data-aos="fade-up" data-aos-duration="300" data-aos-delay="400" data-aos-offset="20">button</button> */}
        </div>
 
@@ -72,7 +74,7 @@ class Home extends Component {
           
        </div>
 
-       <div  id="div-03" > </div>
+       <div  id="div-03" className="background3" > </div>
 
         {/* <div>
           <Paper className ="cardSize" >
