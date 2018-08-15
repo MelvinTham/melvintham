1e4a7c71c3d7d254ab46f6b4089faacc3f727f92
MelvinTham <melvintham1997@gmail.com>
2018-08-15 15:13:50 -0400

    m

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import nyc from './images/nyc.jpg';
@@ -16,6 +16,9 @@ import {Avatar} from '@material-ui/core';
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+      page : '',
+    };
     this.handleScrollToElement = this.handleScrollToElement.bind(this);
   }
 
@@ -42,25 +45,19 @@ class Home extends React.Component {
 			        </a>
           </div>
 
-      
-
                  {/* PROFILE SECTION */}
 
         <div id ="Profile" ref="profile" style={{background:'F4F4F4'}}>
-        
           <div className="flexCenter">
               <h1>Profile</h1>
               <h5>I'm aspiring to be a Software Engineer</h5>
-         
           <hr className="divider"/>
-
           <div className="container"> 
-   <div className="row"> 
-      <div className="col-md-4" style ={{padding:'20px 20px'}}>
-      <h2>About me</h2>
-       <h3> hello   hello   hello   hello   hello   hello   hello   hello   hello   hello   hello   hello   hello</h3>
-      
-      </div>
+           <div className="row"> 
+            <div className="col-md-4" style ={{padding:'20px 20px'}}>
+              <h2>About me</h2>
+              <h3> hello   hello   hello   hello   hello   hello   hello   hello   hello   hello   hello   hello   hello</h3> 
+          </div>
       <div className="col-md-4" style ={{padding:'20px 20px'}}>
       <div className="flexCenter">
       <Avatar  src={melvin} alt ="Melvin" className="melvinStyle"  />
@@ -75,9 +72,15 @@ class Home extends React.Component {
      </div>
     </div>
   </div>
+          {/* Buttons For Different Pages */}
 
+    <div className="pageButtons">
+     <Button variant="outlined" style={{margin:'10px 10px'}}>hello</Button>
+     <Button variant="outlined" style={{margin:'10px 10px'}}>hello</Button>
+     <Button variant="outlined" style={{margin:'10px 10px'}}>hello</Button>
+    </div>
 
-      <div style={{height:'1000px',background:'pink'}}></div>
+      <div style={{height:'1000px',background:'green'}}></div>
   
   
            
